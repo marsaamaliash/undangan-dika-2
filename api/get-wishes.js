8ef0ccf7a02d78ef99de83a568c8863704935644
marsaam <marsaamaliash@gmail.com>
2026-07-09 20:35:28 +0700

    style(api): fix source constant indentation

--- api/get-wishes.js
+++ api/get-wishes.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxV6nu0fGSxQjoHOAFhjlDamQ0XDhwzaM6_isGYHZWgZBETmyoTLJ0a9L3TGVqxhSWN/exec';
-const SOURCE = 'dika-lisa';
+  const SOURCE = 'dika-lisa';
 
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
