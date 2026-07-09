261013c1b333fb6fafaeded8a665ac0c9e3bfca6
marsaam <marsaamaliash@gmail.com>
2026-07-09 21:37:49 +0700

    revert(debug): remove diagnostic URL endpoint

--- api/get-wishes.js
+++ api/get-wishes.js
@@ -2,10 +2,6 @@ export default async function handler(req, res) {
 
   const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxV6nu0fGSxQjoHOAFhjlDamQ0XDhwzaM6_isGYHZWgZBETmyoTLJ0a9L3TGVqxhSWN/exec';
 
-  if (req.query.debug === 'url') {
-    return res.status(200).json({ url: APPS_SCRIPT_URL });
-  }
-
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
